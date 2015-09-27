70f740804ae383a411847a069add4ae84b768511
Stuart Herbert <stuart@stuartherbert.com>
2015-09-27 09:42:18 +0100

    New: expectsHubflowOutput()->isFatalError()

--- storyplayer/php/GanbaroDigital/ST2/Hubflow/ExpectsHubflowOutput.php
+++ storyplayer/php/GanbaroDigital/ST2/Hubflow/ExpectsHubflowOutput.php
@@ -7,6 +7,23 @@ use Prose\E5xx_ExpectFailed;
 
 class ExpectsHubflowOutput
 {
+    public function isFatalError($output)
+    {
+        // what are we doing?
+        $log = usingLog()->startAction("make sure that the output tells the user that a fatal error has occurred");
+
+        // filter the output for the desired text
+        $filteredText = FilterForMatchingString::against($output, 'fatal:');
+
+        if (empty($filteredText)) {
+            $log->endAction("expected text not found");
+            throw new E5xx_ExpectFailed(__METHOD__, "output tells the user that a fatal error has occurred", "output does not tell the user that a fatal error has occurred");
+        }
+
+        // all done
+        $log->endAction();
+    }
+
     public function isRequiresInitialisation($output)
     {
         // what are we doing?
