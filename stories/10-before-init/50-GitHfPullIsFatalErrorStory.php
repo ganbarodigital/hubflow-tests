06c0c88758d4433037e7b42c88694483ff5f90fb
Stuart Herbert <stuart@stuartherbert.com>
2015-09-27 12:08:14 +0100

    New: basic tests for all "push" and "pull" commands against an uninitialised Git repo

---
+++ stories/10-before-init/50-GitHfPullIsFatalErrorStory.php
@@ -0,0 +1,35 @@
+<?php
+
+use Hubflow\Templates\HubflowTestWithClonedLocalRepo;
+
+// ==================================================================
+//
+// When a Git repo has been freshly cloned,
+//     when we run 'git hf pull',
+//     we get a fatal error,
+//     and we are told to initialise Hubflow first
+//
+// ------------------------------------------------------------------
+
+$story = newStoryFor("Hubflow")
+         ->inGroup("Before Initialisation")
+         ->called("Running `git hf pull` results in fatal error")
+         ->basedOn(new HubflowTestWithClonedLocalRepo);
+
+$story->requiresStoryplayerVersion(2);
+
+$story->addAction(function() {
+    $checkpoint = getCheckpoint();
+    $result = usingTestRepo()->runCommandAndIgnoreErrors('git hf pull');
+    $checkpoint->output = $result->output;
+});
+
+$story->addPostTestInspection(function() {
+    // make sure we have some output to examine
+    $checkpoint = getCheckpoint();
+    assertsObject($checkpoint)->hasAttribute('output');
+
+    // make sure the output is as expected
+    expectsHubflowOutput()->isFatalError($checkpoint->output);
+    expectsHubflowOutput()->isRequiresInitialisation($checkpoint->output);
+});
