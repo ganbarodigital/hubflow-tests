042b83d720ea09d944e2b65e20e69ff5221d40ff
Stuart Herbert <stuart@stuartherbert.com>
2015-09-27 12:14:13 +0100

    New: basic tests for all "release" commands against an uninitialised Git repo

---
+++ stories/10-before-init/release/git_hf_release_push_is_fatal_error_Story.php
@@ -0,0 +1,35 @@
+<?php
+
+use Hubflow\Templates\HubflowTestWithClonedLocalRepo;
+
+// ==================================================================
+//
+// When a Git repo has been freshly cloned,
+//     when we run 'git hf release push',
+//     we get a fatal error,
+//     and we are told to initialise Hubflow first
+//
+// ------------------------------------------------------------------
+
+$story = newStoryFor("Hubflow")
+         ->inGroup(["Before Initialisation", 'git hf release'])
+         ->called("Running `git hf release push` results in fatal error")
+         ->basedOn(new HubflowTestWithClonedLocalRepo);
+
+$story->requiresStoryplayerVersion(2);
+
+$story->addAction(function() {
+    $checkpoint = getCheckpoint();
+    $result = usingTestRepo()->runCommandAndIgnoreErrors('git hf release push');
+    $checkpoint->output = $result->output;
+});
+
+$story->addPostTestInspection(function() {
+    // make sure we have some output to examine
+    $checkpoint = getCheckpoint();
+    assertsObject($checkpoint)->hasAttribute('output');
+
+    // make sure the output is as expected
+    expectsHubflowOutput()->isFatalError($checkpoint->output);
+    expectsHubflowOutput()->isRequiresInitialisation($checkpoint->output);
+});
---
+++ stories/10-before-init/release/git_hf_release_start_is_fatal_error_Story.php
@@ -0,0 +1,35 @@
+<?php
+
+use Hubflow\Templates\HubflowTestWithClonedLocalRepo;
+
+// ==================================================================
+//
+// When a Git repo has been freshly cloned,
+//     when we run 'git hf release start',
+//     we get a fatal error,
+//     and we are told to initialise Hubflow first
+//
+// ------------------------------------------------------------------
+
+$story = newStoryFor("Hubflow")
+         ->inGroup(["Before Initialisation", 'git hf release'])
+         ->called("Running `git hf release start` results in fatal error")
+         ->basedOn(new HubflowTestWithClonedLocalRepo);
+
+$story->requiresStoryplayerVersion(2);
+
+$story->addAction(function() {
+    $checkpoint = getCheckpoint();
+    $result = usingTestRepo()->runCommandAndIgnoreErrors('git hf release start');
+    $checkpoint->output = $result->output;
+});
+
+$story->addPostTestInspection(function() {
+    // make sure we have some output to examine
+    $checkpoint = getCheckpoint();
+    assertsObject($checkpoint)->hasAttribute('output');
+
+    // make sure the output is as expected
+    expectsHubflowOutput()->isFatalError($checkpoint->output);
+    expectsHubflowOutput()->isRequiresInitialisation($checkpoint->output);
+});
