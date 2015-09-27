89ba3a9da4387bcdf8c1960d956024d667df81e3
Stuart Herbert <stuart@stuartherbert.com>
2015-09-27 11:59:43 +0100

    New: basic tests for all "feature" commands against an uninitialised Git repo

---
+++ stories/10-before-init/30-GitHfFeatureRebaseIsFatalErrorStory.php
@@ -0,0 +1,34 @@
+<?php
+
+use Hubflow\Templates\HubflowTestWithClonedLocalRepo;
+
+// ==================================================================
+//
+// When a Git repo has been freshly cloned,
+//     we can run 'git hf feature rebase',
+//     and we are told to initialise Hubflow first
+//
+// ------------------------------------------------------------------
+
+$story = newStoryFor("Hubflow")
+         ->inGroup("Before Initialisation")
+         ->called("Running `git hf feature rebase` results in fatal error")
+         ->basedOn(new HubflowTestWithClonedLocalRepo);
+
+$story->requiresStoryplayerVersion(2);
+
+$story->addAction(function() {
+    $checkpoint = getCheckpoint();
+    $result = usingTestRepo()->runCommandAndIgnoreErrors('git hf feature rebase');
+    $checkpoint->output = $result->output;
+});
+
+$story->addPostTestInspection(function() {
+    // make sure we have some output to examine
+    $checkpoint = getCheckpoint();
+    assertsObject($checkpoint)->hasAttribute('output');
+
+    // make sure the output is as expected
+    expectsHubflowOutput()->isFatalError($checkpoint->output);
+    expectsHubflowOutput()->isRequiresInitialisation($checkpoint->output);
+});
